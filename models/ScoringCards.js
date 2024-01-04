777c678f771e5155f8cd921afc66d303b3520952
gruvis41 <dougheinan@gmail.com>
2024-01-04 11:32:36 -0700

    added ScoringCards model

---
+++ models/ScoringCards.js
@@ -0,0 +1,66 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ScoreCardSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String
+    },
+    colOneMulti: {
+        type: Number,
+        requied: true
+    },
+    colOneName: {
+        type: String,
+        required: true
+    },
+    colTwoMulti: {
+        type: Number,
+    },
+    colTwoName: {
+        type: String,
+    },
+    cardTargetScore: {
+        type: Number,
+        required: true
+    },
+    minScore: {
+        type: Number,
+        required: true
+    },
+    maxScore: {
+        type: Number,
+        required: true
+    },
+    startingTotal: {
+        type: Number,
+        required: true
+    },
+    mostPoints: {
+        type: Number
+    },
+    fewestPoints: {
+        type: Number
+    },
+    gamesPlayed: [
+        { 
+            type: Schema.Types.ObjectId,  
+            ref: "GameResult"
+        }
+    ],
+    wins: {
+        type: Number
+    },
+    losses: {
+        type: Number
+    },
+});
+
+module.exports = mongoose.model("ScoreCard", ScoreCardSchema);
