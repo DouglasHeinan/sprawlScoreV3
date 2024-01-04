9d8c86c84f3c1649362caa2693f4ffb3fbbd321f
gruvis41 <dougheinan@gmail.com>
2024-01-04 14:21:22 -0700

    updateUser logic added.

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -13,26 +13,30 @@ const getAllUsers = asyncHandler(async (req, res) => {
     res.json(users);
 });
 
+
 // @desc Create new user
 // @route POST /users
 // @access Private
 const createNewUser = asyncHandler(async (req, res) => {
     const { username, password, email } = req.body;
 
+    // Data confirmation
     if (!username || !password || !email) {
         return res.status(400).json({ message: "All fields are required" });
-    }
+    };
 
-    const duplicate = await User.findOne({ username }).lean().exec()
-
+    // Check for duplicates; 409 for conflict
+    const duplicate = await User.findOne({ username }).lean().exec();
     if (duplicate) {
         return res.status(409).json({ message: "Duplicate username" });
     };
 
+    // Hash password
     const hashedPwd = await bcrypt.hash(password, 10) // salt rounds
 
     const userObject = { username, "password": hashedPwd, email };
 
+    // Create & store user
     const user = await User.create(userObject);
     if (user) {
         res.status(201).json({ message: `New user ${username} created.` });
@@ -41,12 +45,44 @@ const createNewUser = asyncHandler(async (req, res) => {
     }
 });
 
+
 // @desc Update user
 // @route PATCH /users
 // @access Private
 const updateUser = asyncHandler(async (req, res) => {
+    const { id, username, password, email} = req.body;
 
+    // Confirm data
+    if (!id || !username || !email) {
+        return res.status(400).json({ message: "All fields are required" });
+    };
+
+    const user = await User.findById(id).exec();
+
+    if (!user) {
+        return res.status(400).json({ message: "User not found" });
+    };
+
+    // Check for duplicate
+    const duplicate = await User.findOne({ username }).lean().exec();
+    // Allow updates to original user
+    if (duplicate && duplicate?._id.toString() !== id) {
+        return res.status(409).json({ message: "Duplicate username" });
+    };
+
+    user.username = username;
+    user.email = email;
+
+    if (password) {
+        // Hash password
+        user.password = await bcrypt.hash(password, 10) // salt rounds
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({ meassage: `Updated ${updatedUser.username}` })
 });
+
 
 // @desc delete user
 // @route DELETE /users
@@ -54,6 +90,7 @@ const updateUser = asyncHandler(async (req, res) => {
 const deleteUser = asyncHandler(async (req, res) => {
 
 });
+
 
 module.exports = {
     getAllUsers,
