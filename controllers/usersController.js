e2448d621b5056b7a9e0afaa85093f3d868f1e64
gruvis41 <dougheinan@gmail.com>
2024-01-04 13:31:30 -0700

    createNewUser logic added

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -6,14 +6,39 @@ const bcrypt = require("bcrypt");
 // @route GET /users
 // @access Private
 const getAllUsers = asyncHandler(async (req, res) => {
-
+    const users = await User.find().select("-password").lean()
+    if (!users) {
+        return res.status(400).json({ message: "No users found" });
+    }
+    res.json(users);
 });
 
 // @desc Create new user
 // @route POST /users
 // @access Private
 const createNewUser = asyncHandler(async (req, res) => {
+    const { username, password, email } = req.body;
 
+    if (!username || !password || !email) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const duplicate = await User.findOne({ username }).lean().exec()
+
+    if (duplicate) {
+        return res.status(409).json({ message: "Duplicate username" });
+    };
+
+    const hashedPwd = await bcrypt.hash(password, 10) // salt rounds
+
+    const userObject = { username, "password": hashedPwd, email };
+
+    const user = await User.create(userObject);
+    if (user) {
+        res.status(201).json({ message: `New user ${username} created.` });
+    } else {
+        res.status(400).json({ message: "Invalid user data received." })
+    }
 });
 
 // @desc Update user
